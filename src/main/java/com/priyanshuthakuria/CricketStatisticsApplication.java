43a4ed8effd4c4b02936d2925b097ac0a19b338d
Priyanshu Thakuria <priyanshuthakuria@gmail.com>
2021-02-28 00:18:52 +0530

    Commit-2_27-02-2021: added main method

--- src/main/java/com/priyanshuthakuria/CricketStatisticsApplication.java
+++ src/main/java/com/priyanshuthakuria/CricketStatisticsApplication.java
@@ -1,7 +1,11 @@
 package com.priyanshuthakuria;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CricketStatisticsApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CricketStatisticsApplication.class);
+    }
 }
